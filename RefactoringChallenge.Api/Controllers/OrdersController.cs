acdafd8fe2b70ea52b7cdcc769c9559947dbe790
salai-balakumar <salai.balakumar@gmail.com>
2023-06-14 16:18:22 +0100

    WIth additional comments for refactoring and for each method

--- RefactoringChallenge.Api/Controllers/OrdersController.cs
+++ RefactoringChallenge.Api/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using Mapster;
@@ -11,7 +12,7 @@ namespace RefactoringChallenge.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class OrdersController : Controller
+    public class OrdersController : ControllerBase //Used ControllerBase class specifically designed for API controllers in ASP.NET Core
     {
         private readonly NorthwindDbContext _northwindDbContext;
         private readonly IMapper _mapper;
@@ -22,6 +23,12 @@ namespace RefactoringChallenge.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Method to Retrieve list of orders
+        /// </summary>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Get(int? skip = null, int? take = null)
         {
@@ -34,19 +41,21 @@ namespace RefactoringChallenge.Controllers
             {
                 query.Take(take.Value);
             }
-            //var result = _mapper.From(query).ProjectToType<OrderResponse>().ToList();
-            //return Json(result);
-
+            // Used _mapper.Config when calling ProjectToType(), To ensures consistency and clarity in the mapping process.
             var result = query.ProjectToType<OrderResponse>(_mapper.Config).ToList();
-            //var result = query.Adapt<List<OrderResponse>>((TypeAdapterConfig)_mapper);
+            //Ok() is more appropriate and clearer for indicating a successful response.
             return Ok(result);
         }
 
-
+        /// <summary>
+        /// Method to retrieve order based on Id
+        /// </summary>
+        /// <param name="orderId">Input param Orderid</param>
+        /// <returns></returns>
         [HttpGet("{orderId}")]
         public IActionResult GetById([FromRoute] int orderId)
         {
-            //var result = _mapper.From(_northwindDbContext.Orders).ProjectToType<OrderResponse>().FirstOrDefault(o => o.OrderId == orderId);
+            // LINQ query along with Where() method is used to filter the orders based on the orderId directly to improve efficiency
             var result = _northwindDbContext.Orders
                 .Where(o => o.OrderId == orderId)
                 .ProjectToType<OrderResponse>(_mapper.Config)
@@ -54,11 +63,26 @@ namespace RefactoringChallenge.Controllers
 
             if (result == null)
                 return NotFound();
-
-            //return Json(result);
+            
             return Ok(result);
         }
 
+        /// <summary>
+        /// Method to create Order
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <param name="employeeId"></param>
+        /// <param name="requiredDate"></param>
+        /// <param name="shipVia"></param>
+        /// <param name="freight"></param>
+        /// <param name="shipName"></param>
+        /// <param name="shipAddress"></param>
+        /// <param name="shipCity"></param>
+        /// <param name="shipRegion"></param>
+        /// <param name="shipPostalCode"></param>
+        /// <param name="shipCountry"></param>
+        /// <param name="orderDetails"></param>
+        /// <returns></returns>
         [HttpPost("[action]")]
         public IActionResult Create(
             string customerId,
@@ -75,17 +99,14 @@ namespace RefactoringChallenge.Controllers
             IEnumerable<OrderDetailRequest> orderDetails
             )
         {
-            var newOrderDetails = new List<OrderDetail>();
-            foreach (var orderDetail in orderDetails)
+            //Here there is a possibility to create another model class combining all the input parameters for this method but left it for now.
+            var newOrderDetails = orderDetails.Select(orderDetail => new OrderDetail
             {
-                newOrderDetails.Add(new OrderDetail
-                {
-                    ProductId = orderDetail.ProductId,
-                    Discount = orderDetail.Discount,
-                    Quantity = orderDetail.Quantity,
-                    UnitPrice = orderDetail.UnitPrice,
-                });
-            }
+                ProductId = orderDetail.ProductId,
+                Discount = orderDetail.Discount,
+                Quantity = orderDetail.Quantity,
+                UnitPrice = orderDetail.UnitPrice,
+            }).ToList();            
 
             var newOrder = new Order
             {
@@ -106,29 +127,23 @@ namespace RefactoringChallenge.Controllers
             _northwindDbContext.Orders.Add(newOrder);
             _northwindDbContext.SaveChanges();
 
-            //return Json(newOrder.Adapt<OrderResponse>());
             return Ok(newOrder.Adapt<OrderResponse>(_mapper.Config));
         }
 
+        /// <summary>
+        /// Method to Add products to an existing order
+        /// </summary>
+        /// <param name="orderId">Existing Order Id</param>
+        /// <param name="orderDetails">New Order details</param>
+        /// <returns></returns>
         [HttpPost("{orderId}/[action]")]
         public IActionResult AddProductsToOrder([FromRoute] int orderId, IEnumerable<OrderDetailRequest> orderDetails)
         {
+            //Find the order from the DB using OrderId
             var order = _northwindDbContext.Orders.FirstOrDefault(o => o.OrderId == orderId);
             if (order == null)
                 return NotFound();
-
-            //var newOrderDetails = new List<OrderDetail>();
-            //foreach (var orderDetail in orderDetails)
-            //{
-            //    newOrderDetails.Add(new OrderDetail
-            //    {
-            //        OrderId = orderId,
-            //        ProductId = orderDetail.ProductId,
-            //        Discount = orderDetail.Discount,
-            //        Quantity = orderDetail.Quantity,
-            //        UnitPrice = orderDetail.UnitPrice,
-            //    });
-            //}
+            //Get the new order details and create the new list
             var newOrderDetails = orderDetails.Select(orderDetail => new OrderDetail
             {
                 OrderId = orderId,
@@ -140,18 +155,22 @@ namespace RefactoringChallenge.Controllers
 
             _northwindDbContext.OrderDetails.AddRange(newOrderDetails);
             _northwindDbContext.SaveChanges();
-
-            //return Json(newOrderDetails.Select(od => od.Adapt<OrderDetailResponse>()));
+                        
             return Ok(newOrderDetails.Select(od => od.Adapt<OrderDetailResponse>(_mapper.Config)));
         }
 
+        /// <summary>
+        /// Method to delete the order based on OrderId
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
         [HttpPost("{orderId}/[action]")]
         public IActionResult Delete([FromRoute] int orderId)
         {
             var order = _northwindDbContext.Orders.FirstOrDefault(o => o.OrderId == orderId);
             if (order == null)
                 return NotFound();
-
+            //Filter the order based on input orderid
             var orderDetails = _northwindDbContext.OrderDetails.Where(od => od.OrderId == orderId);
 
             _northwindDbContext.OrderDetails.RemoveRange(orderDetails);
--- RefactoringChallenge.Tests/RefactoringChallengeUnitTests.cs
+++ RefactoringChallenge.Tests/RefactoringChallengeUnitTests.cs
@@ -17,9 +17,8 @@ namespace RefactoringChallenge.Tests
 
         public OrdersControllerTests()
         {
-            //_dbContextMock = new Mock<NorthwindDbContext>();
-            //_mapperMock = new Mock<IMapper>();
-            // Create a new instance of Mapper and configure it
+            #region Order
+            // Created a new Orderlist as we mocked DB context and mapper, this is required to test with data as we will not connect to actual DB for unit testing
             orderlist = new List<Order>
             {
                 new Order{ OrderId=1,CustomerId="A1", EmployeeId=1,OrderDate=DateTime.Now,RequiredDate=null,ShippedDate=null,ShipVia=1,Freight=1, ShipName="DHL1", ShipAddress="22", ShipCity="BR",ShipPostalCode="B23",ShipRegion="North",ShipCountry="UK" },
@@ -27,7 +26,6 @@ namespace RefactoringChallenge.Tests
                 new Order{ OrderId=3,CustomerId="A3", EmployeeId=3,OrderDate=DateTime.Now,RequiredDate=null,ShippedDate=null,ShipVia=1,Freight=1, ShipName="DHL1", ShipAddress="42", ShipCity="BR",ShipPostalCode="B43",ShipRegion="West",ShipCountry="UK" },
                 new Order{ OrderId=4,CustomerId="A4", EmployeeId=4,OrderDate=DateTime.Now,RequiredDate=null,ShippedDate=null,ShipVia=1,Freight=1, ShipName="DHL1", ShipAddress="52", ShipCity="BR",ShipPostalCode="B73",ShipRegion="South",ShipCountry="UK" },
             };
-
             var orderQuery = orderlist.AsQueryable();
 
             var dbSetMock = new Mock<DbSet<Order>>();
@@ -36,15 +34,19 @@ namespace RefactoringChallenge.Tests
             dbSetMock.As<IQueryable<Order>>().Setup(m => m.ElementType).Returns(orderQuery.ElementType);
             dbSetMock.As<IQueryable<Order>>().Setup(m => m.GetEnumerator()).Returns(orderQuery.GetEnumerator());
 
+            //Create DB context Mock object
             var _dbContextMock = new Mock<NorthwindDbContext>();
+            //db context mock setup for OrderDetails table from DB
             _dbContextMock.Setup(db => db.Orders).Returns(dbSetMock.Object);
 
+            //Update mapper config profile so that it maps Order and OrderResponse
             var mapperConfig = new TypeAdapterConfig();
             mapperConfig.ForType<Order, OrderResponse>().Map(dest => dest.OrderId, src => src.OrderId);
+            #endregion
 
             #region OrderDetail
-            var orderDetailList = new List<OrderDetail>();
-            
+            // Created a new db context mock with sample input for OrderDetail table
+            var orderDetailList = new List<OrderDetail>();            
             var orderDetailQuery = orderDetailList.AsQueryable();
 
             var dbSetMock1 = new Mock<DbSet<OrderDetail>>();
@@ -53,15 +55,18 @@ namespace RefactoringChallenge.Tests
             dbSetMock1.As<IQueryable<OrderDetail>>().Setup(m => m.ElementType).Returns(orderDetailQuery.ElementType);
             dbSetMock1.As<IQueryable<OrderDetail>>().Setup(m => m.GetEnumerator()).Returns(orderDetailQuery.GetEnumerator());
 
+            //db context mock setup for OrderDetails table from DB
             _dbContextMock.Setup(db => db.OrderDetails).Returns(dbSetMock1.Object);
             
             #endregion
 
-            // Create an IMapper instance using the configured Mapper
+            // Create a controller instance with mapper & DB contextmock
             var mapper = new Mapper(mapperConfig);
             _controller = new OrdersController(_dbContextMock.Object, mapper);
         }
-
+        /// <summary>
+        /// Test method for GET with skip and take
+        /// </summary>
         [Fact]
         public void Get_WithSkipAndTake_ReturnsOrders()
         {
@@ -78,6 +83,9 @@ namespace RefactoringChallenge.Tests
             Assert.Equal(take, orderResponses.Count());
         }
 
+        /// <summary>
+        /// Test method for GetById using OrderId
+        /// </summary>
         [Fact]
         public void GetById_WithExistingOrderId_ReturnsOrder()
         {
@@ -94,6 +102,9 @@ namespace RefactoringChallenge.Tests
             Assert.Equal(expected: expectedOrder.Adapt<OrderResponse>().OrderId,actual: orderResponse.OrderId);
         }
 
+        /// <summary>
+        /// Test Method for GetById using uknown OrderId
+        /// </summary>
         [Fact]
         public void GetById_WithNonExistingOrderId_ReturnsNotFound()
         {
@@ -105,8 +116,11 @@ namespace RefactoringChallenge.Tests
 
             // Assert
             Assert.IsType<NotFoundResult>(result);
-        }        
+        }
 
+        /// <summary>
+        /// Test method for Create
+        /// </summary>
         [Fact]
         public void Create_ReturnsNewOrder()
         {
@@ -124,6 +138,9 @@ namespace RefactoringChallenge.Tests
             Assert.Single(orderResponse.OrderDetails);
         }
 
+        /// <summary>
+        /// Test Method to AddProductsToOrder & return new order details
+        /// </summary>
         [Fact]
         public void AddProductsToOrder_ReturnsNewOrderDetails()
         {
@@ -141,6 +158,9 @@ namespace RefactoringChallenge.Tests
             Assert.Single(orderDetailResponses);
         }
 
+        /// <summary>
+        /// Test Method to delete using OrderId
+        /// </summary>
         [Fact]
         public void Delete_ReturnsOk_WhenOrderDeleted()
         {
